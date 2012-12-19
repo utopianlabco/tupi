02aaeeb512d7b3a9fcd99d98b66e6fc14a2d156e
Gustav Gonzalez <info@maefloresta.com>
2012-12-18 22:36:48 -0500

    Fixing some issues related to the Storyboard feature

--- src/net/tupstoryboardparser.h
+++ src/net/tupstoryboardparser.h
@@ -21,7 +21,7 @@
  *   License:                                                              *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
+ *   the Free Software Foundation; either version 3 of the License, or     *
  *   (at your option) any later version.                                   *
  *                                                                         *
  *   This program is distributed in the hope that it will be useful,       *
@@ -36,29 +36,26 @@
 #ifndef TUPSTORYBOARDPARSER_H
 #define TUPSTORYBOARDPARSER_H
 
-#include "tglobal.h"
-
 #include <QString>
 #include <QDomDocument>
-#include <QTextStream>
 
 /**
    @author Gustav Gonzalez 
 **/
 
-class TUPI_EXPORT TupStoryboardParser
+class TupStoryboardParser
 {
-    public:
-        TupStoryboardParser(const QString &package);
-        ~TupStoryboardParser();
-        bool checksum();
-        int sceneIndex();
-        QString storyboardXml() const;
-        QDomDocument request() const;
+   public:
+      TupStoryboardParser(const QString &package);
+      ~TupStoryboardParser();
+      bool checksum();
+      int sceneIndex();
+      QString storyboardXml() const;
+      QDomDocument request() const;
 
-     private:
-        struct Private;
-        Private *const k;
+   private:
+      struct Private;
+      Private *const k;
 };
 
 #endif
