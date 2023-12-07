96aafb70a2c2e6b195c37b6137e58fb413732c9c
Gustav Gonzalez <xtingray@gmail.com>
2023-12-07 18:48:52 -0500

    - Work in progress: Extending the sound support for the Library panel

--- src/shell/tupmainwindow.cpp
+++ src/shell/tupmainwindow.cpp
@@ -1135,8 +1135,6 @@ bool TupMainWindow::storeProcedure()
             this, SLOT(updateSoundsPath()));
     connect(m_projectManager, SIGNAL(soundPathsChanged()),
             m_libraryWidget, SLOT(updateCurrentSoundPath()));
-    // connect(m_projectManager, SIGNAL(soundPathsChanged()),
-    //         m_libraryWidget, SLOT(updateSoundPlayer()));
 
     if (m_projectManager->saveProject(m_fileName)) {
         updateRecentProjectList();
